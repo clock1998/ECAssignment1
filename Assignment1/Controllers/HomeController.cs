91e3b14be495a2579b4390d10a8522131eea00b1
Lin <Lin>
2018-02-14 21:19:10 -0500

    test1

---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Assignment1DataContext dc = new Assignment1DataContext();
+
+        public ActionResult Index()
+        {
+             var countries = from c in dc.countries
+                        select c;
+             return View();
+        }
+    }
+}
